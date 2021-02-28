e4c529ab1e917f76dad81ae6dffe1a9dd113bc25
Sean <skabe89@gmail.com>
2021-02-28 11:33:01 -0500

    commit tree fc5282dcc371a08d5dd50caa54b7ab19d27210f6

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+
+  }
+
+  static titleize(string){
+    
+  }
+
 }
